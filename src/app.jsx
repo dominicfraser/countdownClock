cb9202f531fc508f0a16e3c5a338a1b3584af3c0
Dominic Fraser <mrdjfraser@gmail.com>
2018-03-05 23:00:50 +0000

    basic template, css

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
+import './app.css';
 
 class App extends Component {
   render() {
-    return <div>Countdown Champ, App Component</div>;
+    return (
+      <div className="app">
+        <div className="app-title">Countdown to December 25, 2018</div>
+        <div>
+          <div className="clock-days">x Days</div>
+          <div className="clock-hours">x Hours</div>
+          <div className="clock-min">x Minutes</div>
+          <div className="clock-sec">x Seconds</div>
+        </div>
+        <div>
+          <input placeholder="new date" />
+          <button>Submit</button>
+        </div>
+      </div>
+    );
   }
 }
 
