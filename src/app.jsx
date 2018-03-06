73f3eefeafa0c984dbd4209a81ef64922cb921d3
Dominic Fraser <mrdjfraser@gmail.com>
2018-03-06 19:43:24 +0000

    initial state updates from form

--- src/app.jsx
+++ src/app.jsx
@@ -2,10 +2,22 @@ import React, { Component } from 'react';
 import './app.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      deadline: 'December 25, 2018',
+      newDeadline: '',
+    };
+  }
+
+  changeDeadline() {
+    this.setState({ deadline: this.state.newDeadline });
+  }
+
   render() {
     return (
       <div className="app">
-        <div className="app-title">Countdown to December 25, 2018</div>
+        <div className="app-title">{this.state.deadline}</div>
         <div>
           <div className="clock-days">x Days</div>
           <div className="clock-hours">x Hours</div>
@@ -13,8 +25,15 @@ class App extends Component {
           <div className="clock-sec">x Seconds</div>
         </div>
         <div>
-          <input placeholder="new date" />
-          <button>Submit</button>
+          <input
+            placeholder="new date"
+            onChange={e =>
+              this.setState({
+                newDeadline: e.target.value,
+              })
+            }
+          />
+          <button onClick={() => this.changeDeadline()}>Submit</button>
         </div>
       </div>
     );
